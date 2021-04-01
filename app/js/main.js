ec23cb7d3eac72a9bc9a67016b5d11db12697b1f
Artur <nasibullin.artur@gmail.com>
2021-04-01 21:57:08 +0500

    refactoring the code, add animation

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,11 @@
 window.addEventListener('DOMContentLoaded', () => {
 	//Smooth Scroll
-
 	function scrollTo() {
-		const links = document.querySelectorAll('.header-menu__link');
-		links.forEach((each) => (each.onclick = scrollAnchors));
+		let headerLinks = document.querySelectorAll('.header-menu__link');
+		let mobileLinks = document.querySelectorAll('.mobile-menu__link');
+		headerLinks.forEach((each) => (each.onclick = scrollAnchors));
+		mobileLinks.forEach((each) => (each.onclick = scrollAnchors));
 	}
-
 	function scrollAnchors(e, respond = null) {
 		const distanceToTop = (el) => Math.floor(el.getBoundingClientRect().top);
 		e.preventDefault();
@@ -24,95 +24,83 @@ window.addEventListener('DOMContentLoaded', () => {
 			}
 		}, 100);
 	}
-	scrollTo();
 
 	// Кнопка menu
-	let btn = document.querySelector('.header__nav-btn');
-	let menu = document.querySelector('.header-menu');
-	let menuItem = document.querySelectorAll('.header-menu__link');
+	let menuBtn = function () {
+		let burger = document.querySelector('.burger');
+		let menu = document.querySelector('.mobile-menu');
+		let menuLinks = document.querySelectorAll('.mobile-menu__link');
 
-	btn.addEventListener('click', () => {
-		menu.classList.toggle('active');
-		btn.classList.toggle('active');
+		burger.addEventListener('click', () => {
+			menu.classList.toggle('active');
+			burger.classList.toggle('active');
 
-		menuItem.forEach((item) => {
-			item.addEventListener('click', () => {
-				btn.classList.remove('active');
-				menu.classList.remove('active');
+			menuLinks.forEach((link) => {
+				link.addEventListener('click', () => {
+					menu.classList.remove('active');
+					burger.classList.remove('active');
+					document.body.style.overflow = '';
+				});
 			});
+			// Блокировать прокрутку экрана при активном Меню
+			if (menu.classList.contains('active')) {
+				document.body.style.overflow = 'hidden';
+			} else {
+				document.body.style.overflow = '';
+			}
 		});
-		// Блокировать прокрутку экрана при активном Меню
-		// if (menu.classList.contains('active')) {
-		// 	document.body.style.overflow = 'hidden';
-		// } else {
-		// 	document.body.style.overflow = '';
-		// }
-	});
+	};
 
-	//Call button
-	let callBtn = document.querySelector('.header-btn__call');
-	if (window.matchMedia('(max-width: 480px)').matches) {
-		callBtn.addEventListener('click', () => {
-			callBtn.classList.toggle('active');
+	//Search button
+	let searchBtnToggle = function () {
+		let searchInput = document.querySelector('.header-form__search');
+		let searchLabel = document.querySelector('.header-form__label');
+		searchLabel.addEventListener('click', (event) => {
+			let target = event.target;
+			if (target.classList.contains('header-form__label')) {
+				searchInput.classList.toggle('active');
+			}
 		});
-	}
+	};
 
 	//Accordion FAQ
-	let faqGrid = document.querySelector('.faq-grid');
-	let plus = document.querySelectorAll('.faq-item__plus');
-	let faqItem = document.querySelectorAll('.faq-item');
+	let accordionFaq = function () {
+		let faqGrid = document.querySelector('.faq-grid');
+		let plus = document.querySelectorAll('.faq-item__plus');
+		let faqItem = document.querySelectorAll('.faq-item');
 
-	const open = (button, dropDown, plus) => {
-		button.classList.add('active');
-		dropDown.classList.add('active');
-		plus.classList.add('active');
-	};
-	const close = (button, dropDown, plus) => {
-		button.classList.remove('active');
-		dropDown.classList.remove('active');
-		plus.classList.remove('active');
-	};
+		const open = (button, dropDown, plus) => {
+			button.classList.add('active');
+			dropDown.classList.add('active');
+			plus.classList.add('active');
+		};
+		const close = (button, dropDown, plus) => {
+			button.classList.remove('active');
+			dropDown.classList.remove('active');
+			plus.classList.remove('active');
+		};
 
-	faqGrid.addEventListener('click', (event) => {
-		let target = event.target;
-		if (target.classList.contains('faq-item__question')) {
-			const parent = target.closest('.faq-item');
-			const answer = parent.querySelector('.faq-item__answer');
-			const plus = parent.querySelector('.faq-item__plus');
-			answer.classList.contains('active') ? close(target, answer, plus) : open(target, answer, plus);
-		}
-	});
-
-	let plusActive = function () {
-		plus.forEach((item) => {
-			item.classList.toggle('active');
+		faqGrid.addEventListener('click', (event) => {
+			let target = event.target;
+			if (target.classList.contains('faq-item__question')) {
+				const parent = target.closest('.faq-item');
+				const answer = parent.querySelector('.faq-item__answer');
+				const plus = parent.querySelector('.faq-item__plus');
+				answer.classList.contains('active') ? close(target, answer, plus) : open(target, answer, plus);
+			}
 		});
 	};
-	let animatedItem = document.querySelectorAll('.animate__animated');
-	animatedItem.forEach((item) => {
-		item.style.opacity = 0;
-		let effect = item.dataset.effect;
-		var waypoint = new Waypoint({
-			element: item,
-			handler: function (direction) {
-				if (effect === 'fadeInUp') {
-					item.classList.add('animate__fadeInUp');
-				} else if (effect === 'fadeInLeft') {
-					item.classList.add('animate__fadeInLeft');
-				} else if (effect === 'fadeInRight') {
-					item.classList.add('animate__fadeInRight');
-				} else if (effect === 'flipInX') {
-					item.classList.add('animate__flipInX');
-					item.style.opacity = 1;
-				} else if (effect === 'zoomIn') {
-					item.classList.add('animate__zoomIn');
-					item.style.opacity = 1;
-				} else if (effect === 'pulse') {
-					item.classList.add('animate__pulse');
-					item.style.opacity = 1;
-				}
-			},
-			offset: '75%',
-		});
+
+	// Input Masks
+	VMasker(document.querySelectorAll('input[type="tel"]')).maskPattern('+9 (999) 999-99-99');
+
+	// Scroll animatios
+	AOS.init({
+		duration: 1500,
 	});
+
+	menuBtn();
+	scrollTo();
+	accordionFaq();
+	searchBtnToggle();
 });
