667580a0c9a5027904fc5a72bbf582b44e357e4a
Artur <nasibullin.artur@gmail.com>
2021-04-02 13:53:48 +0500

    add animations, modal, snowfall

--- app/js/main.js
+++ app/js/main.js
@@ -1,31 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
-	//Smooth Scroll
-	function scrollTo() {
-		let headerLinks = document.querySelectorAll('.header-menu__link');
-		let mobileLinks = document.querySelectorAll('.mobile-menu__link');
-		headerLinks.forEach((each) => (each.onclick = scrollAnchors));
-		mobileLinks.forEach((each) => (each.onclick = scrollAnchors));
-	}
-	function scrollAnchors(e, respond = null) {
-		const distanceToTop = (el) => Math.floor(el.getBoundingClientRect().top);
-		e.preventDefault();
-		var targetID = respond ? respond.getAttribute('href') : this.getAttribute('href');
-		const targetAnchor = document.querySelector(targetID);
-		if (!targetAnchor) return;
-		const originalTop = distanceToTop(targetAnchor);
-		window.scrollBy({ top: originalTop - 30, left: 0, behavior: 'smooth' });
-		const checkIfDone = setInterval(function () {
-			const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
-			if (distanceToTop(targetAnchor) === 0 || atBottom) {
-				targetAnchor.tabIndex = '-1';
-				targetAnchor.focus();
-				window.history.pushState('', '', targetID);
-				clearInterval(checkIfDone);
-			}
-		}, 100);
-	}
-
-	// Кнопка menu
+	// Menu button (mobile)
 	let menuBtn = function () {
 		let burger = document.querySelector('.burger');
 		let menu = document.querySelector('.mobile-menu');
@@ -51,7 +25,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		});
 	};
 
-	//Search button
+	//Search button on 480px screens
 	let searchBtnToggle = function () {
 		let searchInput = document.querySelector('.header-form__search');
 		let searchLabel = document.querySelector('.header-form__label');
@@ -99,8 +73,80 @@ window.addEventListener('DOMContentLoaded', () => {
 		duration: 1500,
 	});
 
+	// Snowfall animations
+	let snowfall = function (block) {
+		origContainer = document.querySelector(block);
+		for (var i = 0; i < 30; i++) {
+			let newDiv = document.createElement('div');
+			newDiv.classList.add('snow');
+			origContainer.prepend(newDiv);
+		}
+	};
+
+	//Smooth Scroll on links
+	function scrollTo() {
+		let headerLinks = document.querySelectorAll('.header-menu__link');
+		let mobileLinks = document.querySelectorAll('.mobile-menu__link');
+		headerLinks.forEach((each) => (each.onclick = scrollAnchors));
+		mobileLinks.forEach((each) => (each.onclick = scrollAnchors));
+	}
+	function scrollAnchors(e, respond = null) {
+		const distanceToTop = (el) => Math.floor(el.getBoundingClientRect().top);
+		e.preventDefault();
+		var targetID = respond ? respond.getAttribute('href') : this.getAttribute('href');
+		const targetAnchor = document.querySelector(targetID);
+		if (!targetAnchor) return;
+		const originalTop = distanceToTop(targetAnchor);
+		window.scrollBy({ top: originalTop - 30, left: 0, behavior: 'smooth' });
+		const checkIfDone = setInterval(function () {
+			const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+			if (distanceToTop(targetAnchor) === 0 || atBottom) {
+				targetAnchor.tabIndex = '-1';
+				targetAnchor.focus();
+				window.history.pushState('', '', targetID);
+				clearInterval(checkIfDone);
+			}
+		}, 100);
+	}
+
+	// Modal windows
+	let modal = () => {
+		let connectBtn = document.querySelectorAll('.modal-open');
+
+		connectBtn.forEach((btn) => {
+			btn.addEventListener('click', () => {
+				if (btn.dataset.connect == 'message') {
+					openModal('.modal__message');
+				} else if (btn.dataset.connect == 'callback') {
+					openModal('.modal__callback');
+				}
+			});
+		});
+		function openModal(window) {
+			let modalWindow = document.querySelector(window);
+			modalWindow.style.opacity = 1;
+			modalWindow.style.visibility = 'visible';
+			document.body.style.overflow = 'hidden';
+		}
+		function closeModal(window) {
+			window.style.opacity = 0;
+			window.style.visibility = 'hidden';
+			document.body.style.overflow = '';
+		}
+		document.addEventListener('click', (event) => {
+			let target = event.target;
+			if (target.classList.contains('modal__close') || target.classList.contains('modal__overlay')) {
+				let parent = target.closest('.modal');
+				closeModal(parent);
+				document.body.style.overflow = '';
+			}
+		});
+	};
+	modal();
 	menuBtn();
 	scrollTo();
 	accordionFaq();
 	searchBtnToggle();
+	snowfall('.hero');
+	snowfall('.ar');
 });
